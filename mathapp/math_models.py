374a6ba5c209e548dfde6b17eedffcbdfbcb4563
unknown <you@example.com>
2023-11-16 21:44:55 +0100

    first math model Addition - is not complete

--- mathapp/math_models.py
+++ mathapp/math_models.py
@@ -4,20 +4,64 @@
 import random
 
 
-class Addition:
-    i = 1
+class MathCase:
     @staticmethod
-    def case(math_operation, numeric_range_low, numeric_range_high):
-        for i in range(10):
+    def addition(numeric_range_low, numeric_range_high):
+        while True:
             a = random.randint(numeric_range_low, numeric_range_high)
             b = random.randint(numeric_range_low, numeric_range_high)
-
-            if (a + b) < numeric_range_high:
-
-                return f"{a}{math_operation}{b} ="
-
-        return None
+            if a + b <= numeric_range_high:
+                return f"{a}+{b} ="
 
 
-first_example = Addition.case(math_operation="+", numeric_range_low=0, numeric_range_high=20)
-print(first_example)
+    @staticmethod
+    def substraction(numeric_range_low, numeric_range_high):
+        a = random.randint(numeric_range_low, numeric_range_high)
+        b = random.randint(numeric_range_low, numeric_range_high)
+        if a > b:
+            return f"{a}-{b} ="
+        else:
+            a = b
+            b = a
+            return f"{a}-{b} ="
+
+
+    @staticmethod
+    def multiplication(numeric_range_low, numeric_range_high):
+        while True:
+            a = random.randint(numeric_range_low, numeric_range_high/10)
+            b = random.randint(numeric_range_low, numeric_range_high/10)
+            if (a*b) != 0 and a != 1 and b != 1:
+                return f"{a}x{b}="
+
+
+
+    @staticmethod
+    def division(numeric_range_low, numeric_range_high):
+        while True:
+            a = random.randint(numeric_range_low, numeric_range_high)
+            b = random.randint(numeric_range_low, numeric_range_high/10)
+            if b != 0 and a != 0 and b != 1 and a <= b*10:
+                if (a % b) == 0:
+                    return f"{a}:{b}="
+
+
+# for i in range(10):
+#     add_examples = MathCase.addition(numeric_range_low=0, numeric_range_high=20)
+#     i += 1
+#     print(add_examples)
+#
+# for i in range(10):
+#     sub_examples = MathCase.substraction(numeric_range_low=0, numeric_range_high=20)
+#     i += 1
+#     print(sub_examples)
+
+# for i in range(10):
+#     multi_examples = MathCase.multiplication(numeric_range_low=0, numeric_range_high=100)
+#     i += 1
+#     print(multi_examples)
+
+for i in range(10):
+    divi_examples = MathCase.division(numeric_range_low=0, numeric_range_high=100)
+    i += 1
+    print(divi_examples)
