5a1910b3419bd9cec447ad81fe5d182a32793088
unknown <you@example.com>
2023-11-20 22:27:26 +0100

    new page with examples and form window.After submit, data are sent to python and print to console

--- mathapp/urls.py
+++ mathapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import generate_math_examples
+from .views import generate_math_examples, validation_math_examples
 
 urlpatterns = [
     path('generate_math_examples/', generate_math_examples, name='generate_math_examples'),
+    path('validation_math_examples/', validation_math_examples, name='validation_math_examples')
 ]
--- mathapp/views.py
+++ mathapp/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from mathapp.math_models import MathCase
-from time import time
+
 
 # Create your views here.
 
 def generate_math_examples(request):
+    # receive a requirement regarding generatin of math examples - typ, range, count
     if request.method == "POST":
-        addition_example = MathCase.addition(count=10, numeric_range_low=0, numeric_range_high=20)
-        print(f"Obsah listu na začátku:{addition_example}")
+        addition_example = MathCase.addition(count=5, numeric_range_low=0, numeric_range_high=20)
+        # print(f"Obsah listu na začátku:{addition_example}")
 
-    #     # place for mathematic examples
+        # place for mathematic examples
         math_examples = {"examples": addition_example}
-        print(math_examples)
+        # print(math_examples)
 
         return render(request, "math_examples.html", context=math_examples)
-    # return render(request, "math_examples.html")
+
+
+
+def validation_math_examples(request):
+    # receive complete generate examples plus results from user. The result are validate
+    if request.method == "POST":
+        results = request.POST.getlist('result')
+        print(f"Přijaté výsledky: {results}")
+        return render(request, 'validation_math_examples.html')
